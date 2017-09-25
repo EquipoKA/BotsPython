6eaca36eec5d67ddc60ebe16bfc9c0c9af1afa6a
MocoNinja <javiergonzaleztello@gmail.com>
2017-09-25 13:35:08 +0200

    Ejemplo holaMundo

---
+++ helloBot.py
@@ -0,0 +1,45 @@
+import json 
+import requests
+
+TOKEN = "402078197:AAHdxsObm-IL6ko0VSlA8QDNGluPr3kiUAE"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+ID_QAZEKE="391707070"
+ID_MOCONINJA="123869263"
+ID_GRUPO="-1001099384957"
+ID_GRUPO_NONAZI="-158272989"
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates():
+    url = URL + "getUpdates"
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+
+def send_message(text, chat_id):
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+mensajes = ["Hola", "Grupo", "de", "clase"]
+for mensaje in mensajes:
+    send_message(mensaje, ID_GRUPO)
+    
