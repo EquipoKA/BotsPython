e2129a950828b30f808a8984a20ef5a7e37427c1
MocoNinja <javiergonzaleztello@gmail.com>
2017-10-16 13:21:54 +0200

    Iniciada lista de IDS

--- res/pics/simpleTest.py
+++ res/pics/simpleTest.py
@@ -1,11 +1,11 @@
 import telegram, random, math
 
 bot = telegram.Bot(token='402078197:AAHdxsObm-IL6ko0VSlA8QDNGluPr3kiUAE')
-
+ID=''
 print(bot.get_me())
-for i in range (0, 10001):
+for i in range (0, 11):
     numeroRandom = 0
-    numeroRandom = int(math.floor(random.uniform(0, 6)))
+    numeroRandom = int(math.floor(random.uniform(1, 6)))
     print(numeroRandom)
     fotoDoge = 'doge{}.jpg'.format(numeroRandom)
-    bot.send_photo(chat_id='-158272989', photo=open(fotoDoge, 'rb'))
+    bot.send_photo(chat_id=ID, photo=open(fotoDoge, 'rb'))
